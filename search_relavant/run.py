db7f05fe8462414d434555ba6247e454fc021a54
陈堉~ <14590334+han_you@user.noreply.gitee.com>
2025-01-10 19:18:16 +0800

    完成IDF和BM25实现

---
+++ search_relavant/run.py
@@ -0,0 +1,6 @@
+from search import  Search
+if __name__=='__main__':
+    search=Search()
+    print("-----------------------------------------------")
+    # print(search.search('腾讯'))
+    print(search.search2('腾讯'))
